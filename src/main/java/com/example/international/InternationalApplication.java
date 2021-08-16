c7b988794f3625aa423582931421e01c2df11e4e
speleo27 <seb10400@orange.fr>
2021-08-16 12:05:08 +0200

    commit to find error

---
+++ src/main/java/com/example/international/InternationalApplication.java
@@ -0,0 +1,50 @@
+package com.example.international;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.Locale;
+
+@Controller
+@SpringBootApplication
+public class InternationalApplication implements WebMvcConfigurer {
+
+    @RolesAllowed("*")
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.FRANCE);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @GetMapping("/international")
+    public String getInternationalPage() {
+        return "thymeleaf/international";
+    }
+
+}
---
+++ src/test/java/com/example/international/internationalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.international;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
